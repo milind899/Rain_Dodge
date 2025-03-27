df25e94be06785ed5d03c82e509a9e86d2f7b4a5
milin <milindshandilya899@gmail.com>
2025-03-27 23:16:38 +0530

    basic BG

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame
+import random
+import time
+
+WIDTH, HEIGHT = 1000, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Rain Dodge')
+
+BG = pygame.image.load('bg.png')
+
+
+def draw():
+    WIN.blit(BG, (0, 0))
+    pygame.display.update()
+
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+            draw()
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
