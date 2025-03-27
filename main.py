08b7f04de83c2563ea0d4ab0a625c83e894d3816
milin <milindshandilya899@gmail.com>
2025-03-27 23:55:41 +0530

    basic movement

--- main.py
+++ main.py
@@ -6,24 +6,42 @@ WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Rain Dodge')
 
-BG = pygame.image.load('bg.png')
+BG = pygame.transform.scale(pygame.image.load('bg.png'), (WIDTH, HEIGHT))
 
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
 
-def draw():
+PLAYER_VEL = 5
+
+def draw(player):
     WIN.blit(BG, (0, 0))
+
+    pygame.draw.rect(WIN, "red", player)
     pygame.display.update()
 
 
 def main():
     run = True
 
+    player = pygame.Rect(200, HEIGHT-PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+
+    clock = pygame.time.Clock()
+
     while run:
+        clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
 
-            draw()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
+            player.x += PLAYER_VEL
+
+        draw(player)
 
     pygame.quit()
 
